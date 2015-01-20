66475492caa347c83a3fc2357dd65cb6591d47d5
lucachaco <luiscarlosch@gmail.com>
2015-01-20 00:02:05 -0600

    First test working with angular.

---
+++ javascripts/app/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/* App Module */
+
+var mangoDescuentoApp = angular.module('mangoDescuentoApp', [
+    'ngRoute',
+    'siteControllers'
+]);
+
+
+/*
+angularApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/saison', {
+                templateUrl: '/assets/app/partials/simple-corners.html',
+                controller: 'TheSeasonsCtrl'
+            }).
+
+            otherwise({
+                redirectTo: '/saison'
+            });
+    }]);
+
+
+angularApp.run(['$rootScope', function ($rootScope) {
+
+
+
+}]);
+*/
---
+++ javascripts/app/controllers.js
@@ -0,0 +1,7 @@
+'use strict';
+var siteControllers = angular.module('siteControllers', []);
+
+siteControllers.controller('DiscountsCtrl', ['$scope',
+    function ($scope) {
+        alert('DiscountsCtrl');
+    }]);
